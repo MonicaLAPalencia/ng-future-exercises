9447b682f4d4a6e9164f016f1fda120ee91e3a0b
MonicaLAPalencia <166110040+MonicaLAPalencia@users.noreply.github.com>
2024-04-11 12:11:36 +0200

    Update hello-world.component.ts

--- src/app/exercises/components/hello-world/hello-world.component.ts
+++ src/app/exercises/components/hello-world/hello-world.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core'
 
 @Component({
+  
   selector: 'app-hello-world',
+  styles: [
+    `
+      .title {
+        color: #blue;
+      }
+    `,
+  ],
+  template: ` <h1>Hello world</h1> `,
   standalone: true,
-  template: ``,
+
 })
-export class HelloWorldComponent {}
